c3c931ea27070d9d06e3147b232b01d662110fed
Andrii <andryxakulishow@gmail.com>
2024-10-21 23:04:56 +0300

    Fix: Move setup middlewares from routers to main

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.config import load_config
-from app.routers import setup_middlewares, setup_routers
+from app.routers import setup_routers
 
 logger = logging.getLogger(__name__)
+
+
+def setup_middlewares(app: FastAPI) -> None:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 def create_app() -> FastAPI:
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from .healthcheck import healthcheck_router
 
 
 def setup_routers(app: FastAPI) -> None:
     app.include_router(healthcheck_router)
-
-
-def setup_middlewares(app: FastAPI) -> None:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
