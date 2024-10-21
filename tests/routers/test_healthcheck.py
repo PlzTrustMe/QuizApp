d78242e67340d4f6f9c1d1772669ac10a4373489
Andrii <andryxakulishow@gmail.com>
2024-10-21 15:26:48 +0300

    Test for healthcheck handler

---
+++ tests/routers/test_healthcheck.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from httpx import Response
+
+
+@pytest.mark.asyncio
+async def test_healthcheck_handler(
+    app: FastAPI,
+    client: TestClient
+):
+    url = app.url_path_for("healthcheck_handler")
+    response: Response = client.get(url=url)
+
+    assert response.is_success
+
+    json_data = response.json()
+
+    assert json_data["detail"] == "ok"
+    assert json_data["result"] == "working"
