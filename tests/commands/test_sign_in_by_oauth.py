27a63631603dea590c0aaeace16e8131d7247b72
Andrii <andryxakulishow@gmail.com>
2024-10-27 22:53:35 +0200

    Test to sign in by oauth command

---
+++ tests/commands/test_sign_in_by_oauth.py
@@ -0,0 +1,21 @@
+from app.core.commands.sign_in_by_oauth import (
+    SignInByOauth,
+    SignInByOauthInputData,
+)
+from app.core.entities.value_objects import UserEmail
+from tests.mocks.commiter import FakeCommiter
+from tests.mocks.user_gateways import FakeUserMapper
+
+
+async def test_sign_in_by_oath(
+    user_gateway: FakeUserMapper,
+    commiter: FakeCommiter,
+    user_email: UserEmail,
+) -> None:
+    command = SignInByOauth(user_gateway, commiter)
+    input_data = SignInByOauthInputData("new_user@gmail.com")
+
+    await command(input_data)
+
+    assert user_gateway.saved
+    assert commiter.commited
