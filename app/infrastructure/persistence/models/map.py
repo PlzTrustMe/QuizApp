877c3a3efdf54737bb04fb0ea2777ba8af4a4ca4
Andrii <andryxakulishow@gmail.com>
2024-10-28 19:50:25 +0200

    Db models

--- app/infrastructure/persistence/models/map.py
+++ app/infrastructure/persistence/models/map.py
@@ -1,5 +1,8 @@
+from .company import map_companies_table, map_company_users_table
 from .user import map_users_table
 
 
 def map_tables() -> None:
     map_users_table()
+    map_companies_table()
+    map_company_users_table()
