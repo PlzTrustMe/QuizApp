8ee1994be439bb1b2783691791f7bc08e7226e96
Andrii <andryxakulishow@gmail.com>
2024-10-28 18:40:23 +0200

    Company user entity

--- app/core/entities/company.py
+++ app/core/entities/company.py
@@ -6,11 +6,18 @@ from app.core.entities.user import UserId
 from app.core.entities.value_objects import CompanyDescription, CompanyName
 
 CompanyId = NewType("CompanyId", int)
+CompanyUserId = NewType("CompanyUserId", int)
 
 
 class Visibility(Enum):
     HIDDEN = auto()
     VISIBLE = auto()
+
+
+class CompanyRole(Enum):
+    OWNER = auto()
+    ADMIN = auto()
+    MEMBER = auto()
 
 
 @dataclass
@@ -20,3 +27,11 @@ class Company:
     name: CompanyName
     description: CompanyDescription
     visibility: Visibility
+
+
+@dataclass
+class CompanyUser:
+    company_user_id: CompanyUserId
+    company_id: CompanyId
+    user_id: UserId
+    role: CompanyRole
