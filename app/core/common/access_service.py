dc076c4abefff6836667219ce1778033aa12c2cc
Andrii <andryxakulishow@gmail.com>
2024-10-30 21:44:48 +0200

    Accept invitation command and test for it

--- app/core/common/access_service.py
+++ app/core/common/access_service.py
@@ -1,6 +1,6 @@
 from app.core.commands.invitation.errors import CompanyUserAlreadyExistError
 from app.core.commands.user.errors import AccessDeniedError
-from app.core.entities.company import Company
+from app.core.entities.company import Company, CompanyId
 from app.core.entities.invitation import Invitation
 from app.core.entities.user import User, UserId
 from app.core.interfaces.company_gateways import CompanyUserGateway
@@ -26,11 +26,11 @@ class AccessService:
         if company.owner_id != actor.user_id:
             raise AccessDeniedError()
 
-    async def _is_not_company_member(self, company: Company, user_id: UserId):
-        if await self.company_user_gateway.is_exist(
-            company.company_id, user_id
-        ):
-            raise CompanyUserAlreadyExistError(company.company_id, user_id)
+    async def _is_not_company_member(
+        self, company_id: CompanyId, user_id: UserId
+    ):
+        if await self.company_user_gateway.is_exist(company_id, user_id):
+            raise CompanyUserAlreadyExistError(company_id, user_id)
 
     async def ensure_can_edit_full_name(self, record_to_edit: User):
         await self._is_identity(record_to_edit.user_id)
@@ -46,14 +46,23 @@ class AccessService:
 
     async def ensure_can_send_invitation(self, company: Company, user: User):
         await self._is_owner(company)
-        await self._is_not_company_member(company, user.user_id)
+        await self._is_not_company_member(company.company_id, user.user_id)
 
     async def ensure_can_reject_invitation(
         self, company: Company, invitation: Invitation
     ):
         await self._is_owner(company) or await self._is_identity(
             invitation.user_id
-        ) and await self._is_not_company_member(company, invitation.user_id)
+        ) and await self._is_not_company_member(
+            company.company_id, invitation.user_id
+        )
+
+    async def ensure_can_accept_invitation(self, invitation: Invitation):
+        await self._is_identity(
+            invitation.user_id
+        ) and await self._is_not_company_member(
+            invitation.company_id, invitation.user_id
+        )
 
     async def ensure_can_send_request(self, company: Company, user: User):
-        await self._is_not_company_member(company, user.user_id)
+        await self._is_not_company_member(company.company_id, user.user_id)
